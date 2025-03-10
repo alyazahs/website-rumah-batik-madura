40dbc860bc93d94e6eafc1f8e0eecf43ef2fe71f
AlyaZahra <alyazahs13@gmail.com>
2025-03-11 06:04:13 +0700

    table

--- database/migrations/2025_03_10_225851_create_orders_table.php
+++ database/migrations/2025_03_10_225851_create_orders_table.php
@@ -13,11 +13,8 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('customer_name');
-            $table->string('customer_phone', 15);
-            $table->json('products'); 
             $table->timestamps();
-        });        
+        });
     }
 
     /**
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('order');
+        Schema::dropIfExists('orders');
     }
 };
