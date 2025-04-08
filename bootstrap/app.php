4580a919882f88d57886d3503626425c8b247f0c
mwhabibi <mohammadwalidhabibi@gmail.com>
2025-04-08 21:30:09 +0700

    login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'auth' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+            // 'auth' => \App\Http\Middleware\RedirectIfAuthenticated::class,
             // 'auth.admin' => \App\Http\Middleware\RedirectIfAuthenticated::class,
             'auth.admin' => \App\Http\Middleware\AdminAuth::class,
+            'auth' => \App\Http\Middleware\AdminAuth::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
