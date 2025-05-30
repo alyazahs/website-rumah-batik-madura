35b95b6f8f7a85152db241e51479992f55572864
mwhabibi <mohammadwalidhabibi@gmail.com>
2025-05-30 14:18:18 +0700

    css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Admin Panel</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <style>
         [x-cloak] {
